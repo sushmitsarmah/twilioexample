2cc98e6bb2c8cc8f413efda1642a05ad09adc25b
sushmit sarmah <sushmitsarmah@gmail.com>
2016-11-13 14:45:49 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # twilioexample
 app using node, angularjs and twilio
+
+### Frontend
+1. Run npm install
+2. Run npm start
+
+### Server
+1. Run npm install
+2. ln -s ../frontend/dist/ dist
+3. npm start
---
+++ frontend/config/webpack.dev.js
@@ -0,0 +1,28 @@
+var webpackMerge = require('webpack-merge');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var commonConfig = require('./webpack.common.js');
+var helpers = require('./helpers');
+
+module.exports = webpackMerge(commonConfig, {
+  devtool: 'cheap-module-eval-source-map',
+
+  output: {
+    path: helpers.root('dist'),
+    publicPath: 'http://localhost:8080/',
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js'
+  },
+
+  plugins: [
+    new ExtractTextPlugin('[name].css'),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })    
+  ],
+
+  devServer: {
+    historyApiFallback: true,
+    stats: 'minimal'
+  }
+});
