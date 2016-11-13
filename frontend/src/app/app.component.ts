683f92f1b5646245cdc150a06049a2461f5e8794
sushmit sarmah <sushmitsarmah@gmail.com>
2016-11-13 22:12:57 +0530

    forms added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,36 @@
 import { Component } from '@angular/core';
 import '../../public/css/styles.styl';
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.styl']
 })
-export class AppComponent { }
+
+export class AppComponent { 
+	imgsrc = '../../public/images/angular.png';
+	heroes = HEROES;
+  	selectedHero: Hero;
+
+  	onSelect(hero: Hero): void {
+	    this.selectedHero = hero;
+	}
+}
